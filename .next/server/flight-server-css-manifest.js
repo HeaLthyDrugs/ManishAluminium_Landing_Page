a65433f20d0f2b3df12ef1d5ee6cd65aad3dd142
Manish Vishwakarma <hbxmanish@gmail.com>
2023-04-24 05:02:47 +0000

    second

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/workspace/Modern_WEB_UI-UX/app/page":["/workspace/Modern_WEB_UI-UX/styles/globals.css"]},"/workspace/Modern_WEB_UI-UX/app/layout.js":["/workspace/Modern_WEB_UI-UX/styles/globals.css"]}
